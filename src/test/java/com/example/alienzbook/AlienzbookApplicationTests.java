f8a9bb3fa65abf2df7a6a1d83e97c2c9d9e5c5a3
AndrewChern <nrnk3373@gmail.com>
2020-08-08 14:27:25 +0300

    initial commit

---
+++ src/test/java/com/example/alienzbook/AlienzbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alienzbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlienzbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
